2c1ad3b37e69a82eba605cdb34958f248e269363
John Flanigan <johnrflanigan@gmail.com>
2026-08-16 00:12:03 -0400

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+
+        for (int i = 0; i < 32; i++) {
+            res = res << 1;
+
+            if ((n & 1) == 1) {
+                res += 1;
+            }
+
+            n = n >> 1;
+        }
+
+        return res;
+    }
+}
