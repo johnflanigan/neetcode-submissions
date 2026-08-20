c09423254d089351a7975260f70db0081a0dbc13
John Flanigan <johnrflanigan@gmail.com>
2026-08-19 21:19:52 -0400

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.java
@@ -0,0 +1,30 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        intervals.sort((i1, i2) -> Integer.compare(i1.start, i2.start));
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int maxRooms = 0;
+
+        for (Interval interval : intervals) {
+            while (!pq.isEmpty() && pq.peek() <= interval.start) {
+                pq.remove();
+            }
+
+            pq.add(interval.end);
+            maxRooms = Math.max(maxRooms, pq.size());
+        }
+
+        return maxRooms;
+    }
+}
